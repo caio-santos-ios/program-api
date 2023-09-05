b4534ec8e4fa5951bd7afd4fc4cd82b55a605d09
caiosantos.dev@outlook.com <seu@email.com>
2023-09-05 12:57:53 -0300

    conectando database

--- src/migrations/1693929416874-initial.ts
+++ src/migrations/1693929416874-initial.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigration1693767836772 implements MigrationInterface {
-    name = 'InitialMigration1693767836772'
+export class Initial1693929416874 implements MigrationInterface {
+    name = 'Initial1693929416874'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "photo" bytea, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
